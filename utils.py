7baae48e073213ee3cb5d7067ce53960e0d883d1
Amirali <132277038+amirali12i@users.noreply.github.com>
2026-05-24 14:58:37 +0330

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,24 @@
+"""Utility functions for parsing, object vocabulary, etc."""
+
+# COCO 80-category object vocabulary
+COCO_OBJECTS = [
+    "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat",
+    "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+    "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack",
+    "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball",
+    "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket",
+    "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
+    "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair",
+    "couch", "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse",
+    "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator",
+    "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"
+]
+
+def extract_objects_from_caption(caption, object_list=COCO_OBJECTS):
+    """Naive keyword extraction; better to use dependency parsing as in paper."""
+    found = []
+    caption_lower = caption.lower()
+    for obj in object_list:
+        if obj in caption_lower:
+            found.append(obj)
+    return found
